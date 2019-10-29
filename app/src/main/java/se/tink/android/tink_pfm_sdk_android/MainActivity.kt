9eef7ce7cf5941d27da7d76cd009c9d0cdd18488
Jan Gillich <jan.gillich@tink.se>
2019-10-29 13:29:20 +0100

    task(project): Android studio project setup

---
+++ app/src/main/java/se/tink/android/tink_pfm_sdk_android/MainActivity.kt
@@ -0,0 +1,12 @@
+package se.tink.android.tink_pfm_sdk_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
