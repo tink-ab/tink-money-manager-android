67adda65448b0f9f8c35167c731d5298da896281
Jan Gillich <jan.gillich@tink.se>
2019-10-30 16:25:34 +0100

    ref(cleanup): Delete all unnecessary files in the 'data' module

--- data/src/main/java/se/tink/converter/ModelConverterImpl.java
+++ data/src/main/java/se/tink/converter/ModelConverterImpl.java
@@ -13,27 +13,13 @@ import se.tink.converter.account.UpdateAccountRequestConverter;
 import se.tink.converter.account.accountDetails.AccountDetailsDTOtoAccountDetailsConverter;
 import se.tink.converter.account.accountDetails.AccountDetailsToAccountDetailsDTOConverter;
 import se.tink.converter.account.loan.LoanToLoanDTOConverter;
-import se.tink.converter.activity.ActivityHtmlHeadResponseConverter;
-import se.tink.converter.activity.ListActivityHtmlResponseToEventsPageConverter;
 import se.tink.converter.authentication.AuthenticationResponseConverter;
 import se.tink.converter.authentication.AuthenticationStatusConverter;
-import se.tink.converter.authentication.InitiateSmsOtpResponseConverter;
-import se.tink.converter.authentication.PhoneNumberAndPin6AuthenticationResponseConverter;
-import se.tink.converter.authentication.ResetPin6ResponseConverter;
-import se.tink.converter.authentication.SmsOtpAndPin6AuthenticationResponseConverter;
 import se.tink.converter.authentication.ThirdPartyAppAuthenticationDTOToThirdPartyAppAuthenticationConverter;
-import se.tink.converter.authentication.VerifySmsOtpResponseConverter;
 import se.tink.converter.budgets.BudgetConverters;
 import se.tink.converter.category.CategoryDTOToCategoryConverter;
 import se.tink.converter.category.CategoryToCategoryDTOConverter;
 import se.tink.converter.category.CategoryTreeDTOToCategoryTreeConverter;
-import se.tink.converter.consents.AvailableConsentsResponseDTOToAvailableConsentsConverter;
-import se.tink.converter.consents.ConsentActionToConsentActionDTOConverter;
-import se.tink.converter.consents.ConsentsDTOToConcentConverter;
-import se.tink.converter.consents.ConsentsDetailsResponseConverter;
-import se.tink.converter.consents.ConsentsMessageDTOToConsentMessageResponseConverter;
-import se.tink.converter.consents.ConsentsMessageLinkDTOToConsentMessageLinkResponseConverter;
-import se.tink.converter.consents.GiveConsentRequestToGiveConsentRequestDTOConverter;
 import se.tink.converter.credential.CachedCredentialToCredentialsEntity;
 import se.tink.converter.credential.CredentialDTOToCredentialConverter;
 import se.tink.converter.credential.CredentialsEntityToCachedCredential;
@@ -62,24 +48,6 @@ import se.tink.converter.follow.SavingsFollowCriteriaDTOToSavingsFollowCriteriaC
 import se.tink.converter.follow.SavingsFollowCriteriaToSavingsFollowCriteriaDTOConverter;
 import se.tink.converter.follow.SearchFollowCriteriaDTOToSearchFollowCriteriaConverter;
 import se.tink.converter.follow.SearchFollowCriteriaToSearchFollowCriteriaDTOConverter;
-import se.tink.converter.identity.IdentityAnswerKeyConverter;
-import se.tink.converter.identity.IdentityAnswerKeyDTOConverter;
-import se.tink.converter.identity.IdentityEventAnswerConverter;
-import se.tink.converter.identity.IdentityEventAnswerFromDTOConverter;
-import se.tink.converter.identity.IdentityEventConverter;
-import se.tink.converter.identity.IdentityEventDocumentationConverter;
-import se.tink.converter.identity.IdentityEventSummaryConverter;
-import se.tink.converter.identity.IdentityStateAddressConverter;
-import se.tink.converter.identity.IdentityStateCompanyConverter;
-import se.tink.converter.identity.IdentityStateCompanyEngagementConverter;
-import se.tink.converter.identity.IdentityStateConverter;
-import se.tink.converter.identity.IdentityStateCreditScoreConverter;
-import se.tink.converter.identity.IdentityStateOutstandingDebtConverter;
-import se.tink.converter.identity.IdentityStatePropertyConverter;
-import se.tink.converter.identity.IdentityStateRecordOfNonPaymentConverter;
-import se.tink.converter.identity.IdentityStateRoleConverter;
-import se.tink.converter.identity.IdentityStateTaxDeclarationConverter;
-import se.tink.converter.kyc.KycAnswersToStoreKycRequestConverter;
 import se.tink.converter.misc.AmountDTOToAmountConverter;
 import se.tink.converter.misc.AmountToAmountDTOConverter;
 import se.tink.converter.misc.ExactNumberDTOToExactNumberConverter;
@@ -90,16 +58,6 @@ import se.tink.converter.misc.ImagesConverter;
 import se.tink.converter.misc.PeriodDTOToPeriodConverter;
 import se.tink.converter.misc.PeriodToPeriodDTOConverter;
 import se.tink.converter.misc.StringToStringValueConverter;
-import se.tink.converter.onboarding.insights.AmountByCategoryCodeDTOToAmountByCategoryConverter;
-import se.tink.converter.onboarding.insights.AmountByWeekdayDTOToAmountByWeekdayConverter;
-import se.tink.converter.onboarding.insights.HistogramBucketDTOToHistogramBucketConverter;
-import se.tink.converter.onboarding.insights.InsightsCategoriesDTOToInsightsCategoriesConverter;
-import se.tink.converter.onboarding.insights.InsightsDailySpendDToToInsightsDailySpendConverter;
-import se.tink.converter.onboarding.insights.InsightsLeftToSpendDTOToInsightsLeftToSpendConverter;
-import se.tink.converter.onboarding.insights.InsightsMortgageDTOToInsightsMortgageConverter;
-import se.tink.converter.onboarding.insights.InsightsResponseToInsightsConverter;
-import se.tink.converter.onboarding.insights.InsightsSavingsDTOToInsightsSavingsConverter;
-import se.tink.converter.onboarding.insights.LeftToSpendByPeriodDTOToLeftToSpendByPeriodConverter;
 import se.tink.converter.provider.ProviderDTOToProviderConverter;
 import se.tink.converter.provider.ProviderToProviderDTOConverter;
 import se.tink.converter.pushnotifications.DeviceFromDTOConverter;
@@ -133,15 +91,6 @@ import se.tink.converter.transaction.TransactionDTOToTransactionConverter;
 import se.tink.converter.transaction.TransactionDetailsDTOToTransactionDetailsConverter;
 import se.tink.converter.transaction.TransactionDetailsToTransactionDetailsDTOConverter;
 import se.tink.converter.transaction.TransactionToTransactionDTOConverter;
-import se.tink.converter.transfer.ClearingLookupResponseToClearingConverter;
-import se.tink.converter.transfer.CreateTransferRequestConverter;
-import se.tink.converter.transfer.GiroLookupEntityDTOToGiroLookupEntityConverter;
-import se.tink.converter.transfer.SignableOperationDTOtoSignableOperationConverter;
-import se.tink.converter.transfer.TransferDTOToTransferConverter;
-import se.tink.converter.transfer.TransferDestinationDTOToTransferDestinationConverter;
-import se.tink.converter.transfer.TransferDestinationPerAccountDTOtoTransferDestinationPerAccount;
-import se.tink.converter.transfer.TransferToTransferDTOConverter;
-import se.tink.converter.transfer.UpdateTransferRequestConverter;
 import se.tink.converter.user.CollectBankIdAuthenticationRequestDTOConverter;
 import se.tink.converter.user.DeleteUserReasonConverter;
 import se.tink.converter.user.EmailAndPasswordAuthenticationResponseDTOConverter;
@@ -215,23 +164,17 @@ public class ModelConverterImpl implements ModelConverter {
         setupTransactionConverters();
         setupSupportModelConverters();
         setupCategoryConverters();
-        setupTransferConverters();
         setupTrackingConverters();
         setupStreamingResponseConverter();
         setupRequestConverters();
         setupFollowItemConverters();
         setupUserConfigurationConverters();
         setupUserConverters();
-        setupActivityConverters();
         setupSettingsConverters();
         setupPushNotificationsConverters();
-        setupInsightsConverters();
         setupDeviceConfigurationConverters();
-        setupConsentConverters();
         setupCacheEntityConverters();
-        setupIdentityConverters();
         setupBudgetConverters();
-        setupKycConverters();
     }
 
     private void addConverter(AbstractConverter abstractConverter) {
@@ -248,39 +191,11 @@ public class ModelConverterImpl implements ModelConverter {
         }
     }
 
-    private void setupIdentityConverters() {
-        addConverter(new IdentityAnswerKeyConverter(this));
-        addConverter(new IdentityEventAnswerConverter(this));
-        addConverter(new IdentityStateConverter(this));
-        addConverter(new IdentityEventSummaryConverter(this));
-        addConverter(new IdentityAnswerKeyDTOConverter(this));
-
-        addConverter(new IdentityStateAddressConverter(this));
-        addConverter(new IdentityStateCompanyEngagementConverter(this));
-        addConverter(new IdentityStateRecordOfNonPaymentConverter(this));
-        addConverter(new IdentityStateCreditScoreConverter(this));
-        addConverter(new IdentityStateTaxDeclarationConverter(this));
-        addConverter(new IdentityStateOutstandingDebtConverter(this));
-
-        addConverter(new IdentityStatePropertyConverter(this));
-        addConverter(new IdentityEventConverter(this));
-
-        addConverter(new IdentityStateRoleConverter(this));
-        addConverter(new IdentityStateCompanyConverter(this));
-        addConverter(new IdentityEventDocumentationConverter(this));
-        addConverter(new IdentityEventAnswerFromDTOConverter(this));
-    }
-
     private void setupAuthenticationConverters() {
         addConverter(new AuthenticationStatusConverter());
-        addConverter(new VerifySmsOtpResponseConverter(this));
-        addConverter(new SmsOtpAndPin6AuthenticationResponseConverter(this));
-        addConverter(new InitiateSmsOtpResponseConverter(this));
         addConverter(new AuthenticationResponseConverter(this));
-        addConverter(new PhoneNumberAndPin6AuthenticationResponseConverter(this));
         addConverter(
                 new ThirdPartyAppAuthenticationDTOToThirdPartyAppAuthenticationConverter());
-        addConverter(new ResetPin6ResponseConverter(this));
     }
 
     private void setupSettingsConverters() {
@@ -290,11 +205,6 @@ public class ModelConverterImpl implements ModelConverter {
         addConverter(new NotificationSettingsConverter(this));
         addConverter(new NotificationSettingsUpdateRequestConverter(this));
         addConverter(new NotificationSettingsResponseConverter(this));
-    }
-
-    private void setupActivityConverters() {
-        addConverter(new ActivityHtmlHeadResponseConverter(this));
-        addConverter(new ListActivityHtmlResponseToEventsPageConverter(this));
     }
 
     private void setupUserConverters() {
@@ -320,18 +230,6 @@ public class ModelConverterImpl implements ModelConverter {
         addConverter(new TrackingViewToTrackingViewDTOConverter(this));
         addConverter(
                 new TrackingConfigurationResponseDTOToTrackingConfigurationResponseConverter());
-    }
-
-    private void setupTransferConverters() {
-        addConverter(new TransferDTOToTransferConverter(this));
-        addConverter(new TransferToTransferDTOConverter(this));
-        addConverter(new CreateTransferRequestConverter(this));
-        addConverter(new UpdateTransferRequestConverter(this));
-        addConverter(new TransferDestinationPerAccountDTOtoTransferDestinationPerAccount(this));
-        addConverter(new TransferDestinationDTOToTransferDestinationConverter(this));
-        addConverter(new SignableOperationDTOtoSignableOperationConverter(this));
-        addConverter(new GiroLookupEntityDTOToGiroLookupEntityConverter(this));
-        addConverter(new ClearingLookupResponseToClearingConverter(this));
     }
 
     private void setupCategoryConverters() {
@@ -450,19 +348,6 @@ public class ModelConverterImpl implements ModelConverter {
         addConverter(new ResponseToRegisterPushNotificationTokenResponseConverter(this));
     }
 
-    private void setupInsightsConverters() {
-        addConverter(new AmountByCategoryCodeDTOToAmountByCategoryConverter(this));
-        addConverter(new AmountByWeekdayDTOToAmountByWeekdayConverter(this));
-        addConverter(new HistogramBucketDTOToHistogramBucketConverter());
-        addConverter(new LeftToSpendByPeriodDTOToLeftToSpendByPeriodConverter());
-        addConverter(new InsightsCategoriesDTOToInsightsCategoriesConverter(this));
-        addConverter(new InsightsDailySpendDToToInsightsDailySpendConverter(this));
-        addConverter(new InsightsLeftToSpendDTOToInsightsLeftToSpendConverter(this));
-        addConverter(new InsightsMortgageDTOToInsightsMortgageConverter(this));
-        addConverter(new InsightsSavingsDTOToInsightsSavingsConverter(this));
-        addConverter(new InsightsResponseToInsightsConverter(this));
-    }
-
     private void setupDeviceConfigurationConverters() {
         addConverter(new DeviceConfigurationMarketResponseConverter(this));
         addConverter(new DeviceConfigurationResponseConverter(this));
@@ -475,25 +360,11 @@ public class ModelConverterImpl implements ModelConverter {
         addConverter(new I18NSettingsConverter());
     }
 
-    private void setupConsentConverters() {
-        addConverter(new ConsentsDetailsResponseConverter(this));
-        addConverter(new ConsentsDTOToConcentConverter(this));
-        addConverter(new ConsentsMessageDTOToConsentMessageResponseConverter(this));
-        addConverter(new ConsentsMessageLinkDTOToConsentMessageLinkResponseConverter(this));
-        addConverter(new AvailableConsentsResponseDTOToAvailableConsentsConverter(this));
-        addConverter(new ConsentActionToConsentActionDTOConverter());
-        addConverter(new GiveConsentRequestToGiveConsentRequestDTOConverter(this));
-    }
-
     private void setupCacheEntityConverters() {
         addConverters(CacheConverterFactory.getCacheConverters(this));
     }
 
     private void setupBudgetConverters() {
         addConverters(BudgetConverters.forConverter(this));
-    }
-
-    private void setupKycConverters() {
-        addConverter(new KycAnswersToStoreKycRequestConverter());
     }
 }
