96d26bf82a5018acad63580d1553689a04f2f000
Magnus Gudmandsen <magnus.gudmandsen@tink.se>
2019-12-03 12:14:16 +0100

    chore(analytics): Remove unused 'TrackerParameter' class

--- pfm-sdk/src/main/java/com/tink/pfmsdk/analytics/TrackerParameter.kt
+++
@@ -1,6 +0,0 @@
-package com.tink.pfmsdk.analytics
-
-enum class TrackerParameter(val parameter: String) {
-    PROVIDER_NAME("provider_name"),
-    FINANCIAL_INSTITUTION_NAME("financial_institution_name")
-}
