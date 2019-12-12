48e98f893737ad64d413bf62aad5757a4d5c1b04
Mohan Kumar <mohan.kumar@tink.se>
2019-12-12 12:44:47 +0100

    task(tracking): Moved screen event setting to OverviewFragment.

--- pfm-sdk/src/main/java/com/tink/pfmsdk/overview/OverviewFragment.kt
+++ pfm-sdk/src/main/java/com/tink/pfmsdk/overview/OverviewFragment.kt
@@ -2,9 +2,11 @@ package com.tink.pfmsdk.overview
 
 import com.tink.pfmsdk.BaseFragment
 import com.tink.pfmsdk.R
+import com.tink.pfmsdk.tracking.ScreenEvent
 
 class OverviewFragment : BaseFragment() {
     override fun getLayoutId(): Int = R.layout.fragment_overview
+    override fun getScreenEvent(): ScreenEvent = ScreenEvent.OVERVIEW
     override fun needsLoginToBeAuthorized(): Boolean = true
     override fun viewReadyAfterLayout(): Boolean = false
     override fun onChildViewReady(child: BaseFragment?) {
