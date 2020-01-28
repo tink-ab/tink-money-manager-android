8c364c1641cc1ed3712e5fb7c0affd40647915aa
Mohan Kumar <mohan.kumar@tink.se>
2020-01-28 16:38:36 +0100

    task(feature-set): Removed parceling boilerplate code

--- pfm-sdk/src/main/java/com/tink/pfmsdk/Features.kt
+++ pfm-sdk/src/main/java/com/tink/pfmsdk/Features.kt
@@ -1,14 +1,13 @@
 package com.tink.pfmsdk
 
-import android.os.Parcel
 import android.os.Parcelable
-import kotlinx.android.parcel.Parceler
 import kotlinx.android.parcel.Parcelize
 
-sealed class OverviewFeature {
+sealed class OverviewFeature : Parcelable {
     @Parcelize
-    class Statistics(val statisticTypes: List<StatisticType>) : OverviewFeature(), Parcelable
+    class Statistics(val statisticTypes: List<StatisticType>) : OverviewFeature()
 
+    @Parcelize
     object LatestTransactions : OverviewFeature()
 }
 
@@ -19,30 +18,7 @@ enum class StatisticType {
 
 @Parcelize
 data class OverviewFeatures(val features: List<OverviewFeature>) : Parcelable {
-
-    companion object : Parceler<OverviewFeatures> {
-
-        private fun readFromParcel(parcel: Parcel): OverviewFeatures {
-            return OverviewFeatures(
-                features = mutableListOf<OverviewFeature>().also {
-                    parcel.readList(
-                        it as List<OverviewFeature>,
-                        OverviewFeature::class.java.classLoader
-                    )
-                }
-            )
-        }
-
-        override fun OverviewFeatures.write(parcel: Parcel, flags: Int) {
-            parcel.apply {
-                writeList(features)
-            }
-        }
-
-        override fun create(parcel: Parcel): OverviewFeatures {
-            return readFromParcel(parcel)
-        }
-
+    companion object {
         val ALL =
             OverviewFeatures(
                 listOf(
