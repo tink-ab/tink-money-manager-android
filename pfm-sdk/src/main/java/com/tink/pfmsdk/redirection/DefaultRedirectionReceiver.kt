da2ddefa47627deb76fe89285dd26f26d0bbf9cb
Mohan Kumar <mohan.kumar@tink.se>
2020-02-26 16:06:03 +0100

    task(insight-action): Handling action to show list of transactions

--- pfm-sdk/src/main/java/com/tink/pfmsdk/redirection/DefaultRedirectionReceiver.kt
+++ pfm-sdk/src/main/java/com/tink/pfmsdk/redirection/DefaultRedirectionReceiver.kt
@@ -3,9 +3,14 @@ package com.tink.pfmsdk.redirection
 import android.content.Context
 import com.tink.pfmsdk.FragmentAnimationFlags
 import com.tink.pfmsdk.FragmentCoordinator
+import com.tink.pfmsdk.R
 import com.tink.pfmsdk.transaction.CategorizationFlowFragment
+import com.tink.pfmsdk.transaction.StatusSubtitleMode
+import com.tink.pfmsdk.transaction.TransactionsListFragment
+import com.tink.pfmsdk.transaction.TransactionsListMetaData
 import se.tink.android.di.application.ApplicationScoped
 import se.tink.android.redirection.RedirectionReceiver
+import se.tink.commons.extensions.getColorFromAttr
 import se.tink.core.models.misc.Amount
 import javax.inject.Inject
 
@@ -26,7 +31,21 @@ internal class DefaultRedirectionReceiver @Inject constructor(
     }
 
     override fun showTransactionListForIds(transactionIds: List<String>) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        fragmentCoordinator.replace(
+            TransactionsListFragment.newInstance(
+                data = TransactionsListMetaData(
+                    statusBarColor = context.getColorFromAttr(R.attr.tink_colorPrimaryDark),
+                    backgroundColor = context.getColorFromAttr(R.attr.tink_colorPrimary),
+                    title = context.getString(R.string.transactions_list_toolbar_title),
+                    period = null,
+                    categoryCode = null,
+                    isShowAll = false,
+                    statusSubtitleMode = StatusSubtitleMode.SHOW_REDUCED_AMOUNT,
+                    transactionIds = transactionIds,
+                    accountId = null
+                )
+            )
+        )
     }
 
     override fun showTransactionDetails(transactionId: String) {
