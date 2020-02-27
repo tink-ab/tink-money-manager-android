1d89ffa792e10a6a96c01d0429a9ec60c12578e3
Mohan Kumar <mohan.kumar@tink.se>
2020-02-27 16:04:35 +0100

    task(insight-action): Converted custom action handling into open class
    So customers can just override the actions they need to.

--- pfm-sdk/src/main/java/com/tink/pfmsdk/insights/actionhandling/TinkInsightActionHandler.kt
+++ pfm-sdk/src/main/java/com/tink/pfmsdk/insights/actionhandling/TinkInsightActionHandler.kt
@@ -2,7 +2,27 @@ package com.tink.pfmsdk.insights.actionhandling
 
 import se.tink.core.models.misc.Amount
 
-interface TinkInsightActionHandler {
+open class TinkInsightActionHandler : InsightActionHandler {
+    override fun viewBudget(budgetId: String, periodStart: String): Boolean = false
+
+    override fun categorizeTransaction(transactionId: String): Boolean = false
+
+    override fun viewTransactions(transactionIds: List<String>): Boolean = false
+
+    override fun viewTransactionsByCategory(
+        transactionsByCategory: Map<String, List<String>>
+    ): Boolean = false
+
+    override fun showTransfer(
+        sourceUri: String,
+        destinationUri: String,
+        amount: Amount
+    ): Boolean = false
+
+    override fun showCategory(categoryCode: String): Boolean = false
+}
+
+internal interface InsightActionHandler {
     fun viewBudget(budgetId: String, periodStart: String): Boolean
     fun categorizeTransaction(transactionId: String): Boolean
     fun viewTransactions(transactionIds: List<String>): Boolean
