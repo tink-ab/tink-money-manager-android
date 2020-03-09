bbdb1e8f8dc5840f55e6c298d61ed3c9ca9f2f01
Mohan Kumar <mohan.kumar@tink.se>
2020-03-09 16:33:09 +0100

    task(insight-action): Made action handler methods open for overriding

--- pfm-sdk/src/main/java/com/tink/pfmsdk/insights/actionhandling/InsightActionHandler.kt
+++ pfm-sdk/src/main/java/com/tink/pfmsdk/insights/actionhandling/InsightActionHandler.kt
@@ -3,17 +3,17 @@ package com.tink.pfmsdk.insights.actionhandling
 import se.tink.core.models.misc.Amount
 
 open class InsightActionHandler {
-    fun viewBudget(budgetId: String, periodStart: String): Boolean = false
+    open fun viewBudget(budgetId: String, periodStart: String): Boolean = false
 
-    fun categorizeTransaction(transactionId: String): Boolean = false
+    open fun categorizeTransaction(transactionId: String): Boolean = false
 
-    fun viewTransactions(transactionIds: List<String>): Boolean = false
+    open fun viewTransactions(transactionIds: List<String>): Boolean = false
 
-    fun viewTransactionsByCategory(
+    open fun viewTransactionsByCategory(
         transactionsByCategory: Map<String, List<String>>
     ): Boolean = false
 
-    fun showTransfer(
+    open fun showTransfer(
         sourceUri: String,
         destinationUri: String,
         amount: Amount
