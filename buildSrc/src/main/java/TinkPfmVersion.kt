e426013afb6d53419216803c03fa516a95829a24
Mohan Kumar <mohan.kumar@tink.se>
2020-03-10 16:42:47 +0100

    task(version): Changed version to 0.9.0

--- buildSrc/src/main/java/TinkPfmVersion.kt
+++ buildSrc/src/main/java/TinkPfmVersion.kt
@@ -1,7 +1,7 @@
 object TinkPfmVersion {
 
-    private const val major = 1
-    private const val minor = 0
+    private const val major = 0
+    private const val minor = 9
     private const val patch = 0
 
     const val name = "$major.$minor.$patch"
