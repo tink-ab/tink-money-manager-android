4794fbaba68a2db1ade8a2e09140d860d253e13e
Jan Gillich <jan.gillich@tink.se>
2020-03-26 13:04:17 +0100

    feat(core setup): Add some missing category utilities

--- commons/src/main/java/se/tink/commons/extensions/CategoryExtensions.kt
+++ commons/src/main/java/se/tink/commons/extensions/CategoryExtensions.kt
@@ -3,22 +3,37 @@ package se.tink.commons.extensions
 import com.tink.model.category.Category
 import com.tink.model.category.CategoryTree
 
-fun CategoryTree.findCategoryByCode(code: String): Category {
-    TODO("Core setup")
+fun CategoryTree.findCategoryByCode(code: String): Category? =
+    findRecursive(expenses, code)
+        ?: findRecursive(income, code)
+        ?: findRecursive(transfers, code)
+
+fun findRecursive(category: Category, categoryCode: String): Category? {
+    if (category.code == categoryCode) {
+        return category
+    }
+    if (category.children.isEmpty()) {
+        return null
+    }
+    for (childCategory in category.children) {
+        val returnCategory = findRecursive(childCategory, categoryCode)
+        if (returnCategory != null) {
+            return returnCategory
+        }
+    }
+    return null
 }
 
-fun CategoryTree.getCategoryByType(type: Category.Type): Category {
-    TODO("Core setup")
-}
-
-
-val Category.type: Category.Type get() {
-    TODO("Core setup")
-}
+fun CategoryTree.getCategoryByType(type: Category.Type): Category =
+    when (type) {
+        Category.Type.INCOME -> income
+        Category.Type.EXPENSE -> expenses
+        Category.Type.TRANSFER -> transfers
+    }
 
 val Category.parent: Category
     get() {
-    TODO("Core setup")
+    TODO("Core setup - how should we implement this?")
 }
 
 fun Category.getNameWithDefaultChildFormat(defaultChildFormat: String?): String? {
