ee9459caa0e17165264349735a53b6477ac0fd7e
Mohan Kumar <mohan.kumar@tink.se>
2020-05-26 13:07:12 +0200

    feat(amount): Added amount extensions for exact amount formatting

--- pfm-ui/src/main/java/com/tink/pfmui/util/extensions/AmountExt.kt
+++ pfm-ui/src/main/java/com/tink/pfmui/util/extensions/AmountExt.kt
@@ -7,18 +7,38 @@ internal fun Amount.formatCurrencyRound(): String? {
     return CurrencyUtils.formatCurrencyRound(this)
 }
 
+internal fun Amount.formatCurrencyExact(): String? {
+    return CurrencyUtils.formatCurrencyExact(this)
+}
+
 internal fun Amount.formatCurrencyRoundWithoutSignAndSymbol(): String? {
     return CurrencyUtils.formatCurrencyRoundWithoutSignAndSymbol(this)
+}
+
+internal fun Amount.formatCurrencyExactWithoutSignAndSymbol(): String? {
+    return CurrencyUtils.formatCurrencyExactWithoutSignAndSymbol(this)
 }
 
 internal fun Amount.formatCurrencyRoundWithoutSign(): String? {
     return CurrencyUtils.formatCurrencyRoundWithoutSign(this)
 }
 
+internal fun Amount.formatCurrencyExactWithoutSign(): String? {
+    return CurrencyUtils.formatCurrencyExactWithoutSign(this)
+}
+
 internal fun Amount.formatCurrencyRoundWithoutSymbol(): String? {
-    return CurrencyUtils.formatCurrencyRoundWithoutSymbol(this)
+    return CurrencyUtils.formatCurrencyExactWithoutSymbol(this)
+}
+
+internal fun Amount.formatCurrencyExactWithoutSymbol(): String? {
+    return CurrencyUtils.formatCurrencyExactWithoutSymbol(this)
 }
 
 internal fun Amount.formatCurrencyExplicitlyPositive(): String? {
     return CurrencyUtils.formatCurrencyWithExplicitPositive(this)
+}
+
+internal fun Amount.formatCurrencyExactExplicitlyPositive(): String? {
+    return CurrencyUtils.formatCurrencyExactWithExplicitPositive(this)
 }
