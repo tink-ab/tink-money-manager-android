bf29e359e23b2770f290719571f1bdcdf5bc6319
Jan Gillich <jan.gillich@tink.se>
2020-07-20 18:21:00 +0200

    feat(statistics over time): Create bar-chart with recycler view

---
+++ pfm-ui/src/main/java/com/tink/pfmui/overview/charts/PeriodBalanceBarChartAdapter.kt
@@ -0,0 +1,69 @@
+package com.tink.pfmui.overview.charts
+
+import android.content.res.ColorStateList
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.tink.pfmui.R
+import com.tink.pfmui.charts.models.PeriodBalance
+import com.tink.pfmui.util.CurrencyUtils
+import kotlinx.android.synthetic.main.view_period_balance_bar_chart_item.view.*
+import org.joda.time.DateTime
+import se.tink.commons.extensions.inflate
+import se.tink.core.models.misc.Period
+import kotlin.random.Random
+
+class PeriodBalanceBarChartAdapter : RecyclerView.Adapter<BarChartItemViewHolder>() {
+
+    //TODO: Fill with real data
+    var items: List<PeriodBalance> = run {
+        val now = DateTime.now()
+        (0..24).map {
+            PeriodBalance(
+                Period.createMonthPeriod(now.minusMonths(it)),
+                Random.nextDouble(0.0, 100.0)
+            )
+        }
+    }
+
+    var max = items.map { it.amount }.max() ?: 0.0
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BarChartItemViewHolder =
+        BarChartItemViewHolder(parent)
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: BarChartItemViewHolder, position: Int) {
+        holder.bind(items[position], max)
+    }
+}
+
+class BarChartItemViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+    parent.inflate(R.layout.view_period_balance_bar_chart_item)
+) {
+    fun bind(periodBalance: PeriodBalance, maxAmount: Double) {
+
+        val factor = (periodBalance.amount / maxAmount).toFloat()
+
+        with(itemView) {
+            (bar.layoutParams as? ConstraintLayout.LayoutParams)?.let {
+                it.verticalWeight = factor
+                bar.layoutParams = it
+            }
+
+            (spacer.layoutParams as? ConstraintLayout.LayoutParams)?.let {
+                it.verticalWeight = 1 - factor
+                spacer.layoutParams = it
+            }
+
+            bar.backgroundTintList = ColorStateList.valueOf(
+                ContextCompat.getColor(context, R.color.expensive_blue) // TODO: Color configuration
+            )
+
+            periodLabel.text = periodBalance.period?.toMonthString() //TODO: Period formatting
+            amountLabel.text =
+                CurrencyUtils.formatAmountExactWithCurrencySymbol(periodBalance.amount)
+        }
+    }
+}
--- pfm-ui/src/main/java/com/tink/pfmui/overview/charts/StatisticsOverTimeFragment.kt
+++ pfm-ui/src/main/java/com/tink/pfmui/overview/charts/StatisticsOverTimeFragment.kt
@@ -1,9 +1,24 @@
 package com.tink.pfmui.overview.charts
 
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.tink.pfmui.BaseFragment
 import com.tink.pfmui.R
+import kotlinx.android.synthetic.main.fragment_statistics_over_time.*
 
 class StatisticsOverTimeFragment : BaseFragment() {
     override fun needsLoginToBeAuthorized(): Boolean = true
     override fun getLayoutId(): Int = R.layout.fragment_statistics_over_time
+
+    val adapter = PeriodBalanceBarChartAdapter()
+
+    override fun authorizedOnViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.authorizedOnViewCreated(view, savedInstanceState)
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager =
+            LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, true)
+    }
 }
