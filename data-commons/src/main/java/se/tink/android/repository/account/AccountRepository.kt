42fac06936c3c4d8273e11a0a2bbff4332016aa1
Jan Gillich <jan.gillich@tink.se>
2020-07-30 14:13:47 +0200

    feat(pfm core): Update account and user repositories with coroutines

--- data-commons/src/main/java/se/tink/android/repository/account/AccountRepository.kt
+++ data-commons/src/main/java/se/tink/android/repository/account/AccountRepository.kt
@@ -1,22 +1,23 @@
 package se.tink.android.repository.account
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.liveData
 import com.tink.annotations.PfmScope
 import com.tink.model.account.Account
 import com.tink.service.account.AccountService
-import se.tink.android.livedata.ErrorOrValue
-import se.tink.android.livedata.createResultHandler
 import se.tink.android.livedata.map
 import javax.inject.Inject
 
 @PfmScope
 class AccountRepository @Inject constructor(private val accountService: AccountService) {
 
-    fun accounts(): LiveData<List<Account>> {
-        val liveData = MutableLiveData<ErrorOrValue<List<Account>>>()
-        accountService.listAccounts(liveData.createResultHandler())
-        return liveData.map { it.value ?: emptyList() }
+    fun accounts(): LiveData<List<Account>> = liveData {
+        val accounts = try {
+            accountService.listAccounts()
+        } catch (t: Throwable) {
+            emptyList<Account>()
+        }
+        emit(accounts)
     }
 
     fun accountById(id: String): LiveData<Account?> = accounts().map { accountList ->
