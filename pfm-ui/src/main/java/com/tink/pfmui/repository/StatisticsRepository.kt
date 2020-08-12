be680df4723606c4cf91f27834d5a36f6ef02bc4
Mohan Kumar <mohan.kumar@tink.se>
2020-08-12 14:28:48 +0200

    feat(pfm-core): Refresh statistics upon transaction update events

--- pfm-ui/src/main/java/com/tink/pfmui/repository/StatisticsRepository.kt
+++ pfm-ui/src/main/java/com/tink/pfmui/repository/StatisticsRepository.kt
@@ -2,6 +2,7 @@ package com.tink.pfmui.repository
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import com.tink.annotations.PfmScope
 import com.tink.model.statistics.Statistics
@@ -10,59 +11,61 @@ import com.tink.service.statistics.StatisticsQueryDescriptor
 import com.tink.service.statistics.StatisticsService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import org.joda.time.DateTime
-import se.tink.android.livedata.AutoFetchLiveData
 import se.tink.android.livedata.map
-import se.tink.android.livedata.switchMap
 import se.tink.android.repository.service.DataRefreshHandler
 import se.tink.android.repository.service.Refreshable
+import se.tink.android.repository.transaction.TransactionUpdateEventBus
 import se.tink.android.repository.user.UserRepository
 import se.tink.commons.extensions.isInPeriod
 import javax.inject.Inject
 
+@ExperimentalCoroutinesApi
 @PfmScope
 internal class StatisticsRepository @Inject constructor(
     private val statisticsService: StatisticsService,
     dataRefreshHandler: DataRefreshHandler,
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    transactionUpdateEventBus: TransactionUpdateEventBus
 ) : Refreshable {
     private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
     init {
         dataRefreshHandler.registerRefreshable(this)
+        transactionUpdateEventBus.subscribe { refresh() }
     }
 
-    // TODO: Don't expose LiveData directly from a repository. They belong in ViewModels.
-    private val _statistics = MediatorLiveData<List<Statistics>>()
-    val statistics: LiveData<List<Statistics>> = _statistics
+    private val refreshTrigger = MutableLiveData<Unit>()
 
-    private var dataSource: LiveData<List<Statistics>>? = null
+    // TODO: Don't expose LiveData directly from a repository. They belong in ViewModels.
+    private val statisticsLiveData = MediatorLiveData<List<Statistics>>().apply {
+        fun update() {
+            val userProfile = userRepository.userProfile.value ?: return
+            scope.launch {
+                val result: List<Statistics> = try {
+                    statisticsService.query(
+                        StatisticsQueryDescriptor(
+                            userProfile.periodMode,
+                            userProfile.currency
+                        )
+                    )
+                } catch (error: Throwable) {
+                    emptyList()
+                }
+                postValue(result)
+            }
+        }
+        addSource(userRepository.userProfile) { update() }
+        addSource(refreshTrigger) { update() }
+    }
+
+    val statistics: LiveData<List<Statistics>> = statisticsLiveData
 
     private val periodMap: LiveData<Map<String, Period>> = statistics.map { statistics ->
         statistics.associate { it.period.identifier to it.period  }
-    }
-
-    private fun fetch(): LiveData<List<Statistics>> {
-        return userRepository.userProfile.switchMap { userProfile ->
-            AutoFetchLiveData<List<Statistics>> {
-                scope.launch {
-                    val result: List<Statistics> = try {
-                        val statistics = statisticsService.query(
-                            StatisticsQueryDescriptor(
-                                userProfile!!.periodMode,
-                                userProfile.currency
-                            )
-                        )
-                        statistics
-                    } catch (error: Throwable) {
-                        emptyList()
-                    }
-                    it.postValue(result)
-                }
-            }
-        }
     }
 
     val periods: LiveData<List<Period>> = Transformations.map(periodMap) {
@@ -76,14 +79,7 @@ internal class StatisticsRepository @Inject constructor(
     }
 
     override fun refresh() {
-        dataSource?.let {
-            _statistics.removeSource(it)
-        }
-        val newSource = fetch()
-        _statistics.addSource(newSource) {
-            _statistics.postValue(it)
-        }
-        dataSource = newSource
+        refreshTrigger.postValue(Unit)
     }
 }
 
