8bf4eb68e169588be2c43f1a0645b45b2be5f960
Mohan Kumar <mohan.kumar@tink.se>
2020-08-12 14:45:28 +0200

    feat(pfm-core): Made account repository refreshable

--- data-commons/src/main/java/se/tink/android/repository/account/AccountRepository.kt
+++ data-commons/src/main/java/se/tink/android/repository/account/AccountRepository.kt
@@ -1,26 +1,48 @@
 package se.tink.android.repository.account
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.liveData
 import com.tink.annotations.PfmScope
 import com.tink.model.account.Account
 import com.tink.service.account.AccountService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import se.tink.android.livedata.AutoFetchLiveData
 import se.tink.android.livedata.map
+import se.tink.android.repository.service.DataRefreshHandler
+import se.tink.android.repository.service.Refreshable
 import javax.inject.Inject
 
 @PfmScope
-class AccountRepository @Inject constructor(private val accountService: AccountService) {
+class AccountRepository @Inject constructor(
+    private val accountService: AccountService,
+    dataRefreshHandler: DataRefreshHandler
+) : Refreshable {
+    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
-    fun accounts(): LiveData<List<Account>> = liveData {
-        val accounts = try {
-            accountService.listAccounts()
-        } catch (t: Throwable) {
-            emptyList<Account>()
-        }
-        emit(accounts)
+    init {
+        dataRefreshHandler.registerRefreshable(this)
     }
 
-    fun accountById(id: String): LiveData<Account?> = accounts().map { accountList ->
+    private val _accounts: AutoFetchLiveData<List<Account>> =
+        AutoFetchLiveData {
+            scope.launch {
+                val accounts = try {
+                    accountService.listAccounts()
+                } catch (t: Throwable) {
+                    emptyList<Account>()
+                }
+                it.postValue(accounts)
+            }
+        }
+    val accounts: LiveData<List<Account>> = _accounts
+
+    fun accountById(id: String): LiveData<Account?> = accounts.map { accountList ->
         accountList.firstOrNull { it.id == id }
+    }
+
+    override fun refresh() {
+        _accounts.update()
     }
 }
