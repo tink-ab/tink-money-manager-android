a7aff35d9bf5258a6a9efdbd81cc94e3a981cc96
Mohan Kumar <mohan.kumar@tink.se>
2020-08-18 16:19:05 +0200

    refactor(statistics-monthly): Disable amount text transition
    Same as in other screens, disabling this until we figure out how to do
    this properly with decimals.

--- pfm-ui/src/main/java/com/tink/pfmui/overview/charts/piechart/FullPieChartFragment.kt
+++ pfm-ui/src/main/java/com/tink/pfmui/overview/charts/piechart/FullPieChartFragment.kt
@@ -129,11 +129,12 @@ internal class FullPieChartFragment : BaseFragment() {
         addTransition(PieChartTransition())
         addTransition(PieChartSegmentTransition(R.id.transition_group_main))
         addTransition(Fade().apply { addTarget(R.id.back_segment) })
-        addTransition(TextAmountTransition(CurrencyUtils.getMinusSign()) {
-            amountFormatter.format(it.toDouble(), useSymbol = false)
-        }.apply {
-            addTarget(R.id.amount)
-        })
+        // TODO: Fix this once we have figured out how to do amount transitions for floating point numbers
+//        addTransition(TextAmountTransition(CurrencyUtils.getMinusSign()) {
+//            amountFormatter.format(it.toDouble(), useSymbol = false)
+//        }.apply {
+//            addTarget(R.id.amount)
+//        })
 
         val labelCategories = data.items.map { it.category.code }
         if (labelCategories.isNotEmpty()) {
