f10e7f8086bdbb1120ca50f3f756553f51096453
Mohan Kumar <mohan.kumar@tink.se>
2021-01-05 17:43:22 +0100

    fix(pfm-ui): Remove supported languages logic and use device locale
    The device locale will be used for months (period) and date headers etc.
    The customer can still force set a device locale on the containing activity context
    to override the default device locale.

--- pfm-ui/src/main/java/com/tink/pfmui/configuration/SuitableLocaleFinder.kt
+++ pfm-ui/src/main/java/com/tink/pfmui/configuration/SuitableLocaleFinder.kt
@@ -3,10 +3,8 @@ package com.tink.pfmui.configuration
 import android.content.Context
 import android.content.res.Resources
 import androidx.core.os.ConfigurationCompat
-import com.tink.pfmui.buildConfig.BuildConfigurations.instance
+import androidx.core.os.LocaleListCompat
 import com.tink.pfmui.di.FragmentScoped
-import se.tink.android.di.application.ApplicationScoped
-import java.util.ArrayList
 import java.util.Locale
 import javax.inject.Inject
 
@@ -16,52 +14,13 @@ internal class SuitableLocaleFinder @Inject constructor(
 
     private val resources: Resources = context.resources
 
-    private val localesSupportedByApp: List<Locale>
-        get() {
-            val locales: MutableList<Locale> =
-                ArrayList()
-            for (languageTag in instance.supportedLangugaes) {
-                val dashedLanguageTag =
-                    convertLanguageTagToDashedLanguageTag(languageTag)
-                locales.add(Locale.forLanguageTag(dashedLanguageTag))
-            }
-            return locales
+    private val localesOnDevice: LocaleListCompat
+        get() = ConfigurationCompat.getLocales(resources.configuration)
+
+    fun findLocale(): Locale =
+        if (!localesOnDevice.isEmpty) {
+            localesOnDevice[0]
+        } else {
+            Locale.getDefault()
         }
-
-    private val localeOnDevice: Locale
-        get() = ConfigurationCompat.getLocales(resources.configuration)[0]
-
-    fun findLocaleString(): String = getLocaleAsUnderscoredLanguageTag(findLocale())
-
-    fun findLocale(): Locale {
-        val localesSupportedByApp = localesSupportedByApp
-        val localesOnDevice = localeOnDevice
-        return findMostSuitableLocale(localesSupportedByApp, localesOnDevice)
-    }
-
-    private fun convertLanguageTagToDashedLanguageTag(languageTag: String): String =
-        languageTag.replace("_", "-")
-
-    private fun findMostSuitableLocale(
-        localesSupportedByApp: List<Locale>,
-        localeOnDevice: Locale
-    ): Locale {
-        val localeWithSupportedLanguage = findLocaleWithLanguage(
-            localesSupportedByApp,
-            localeOnDevice.language
-        )
-        return localeWithSupportedLanguage ?: localesSupportedByApp[0]
-    }
-
-    private fun findLocaleWithLanguage(locales: List<Locale>, language: String): Locale? {
-        for (locale in locales) {
-            if (locale.language.equals(language, ignoreCase = true)) {
-                return locale
-            }
-        }
-        return null
-    }
-
-    private fun getLocaleAsUnderscoredLanguageTag(locale: Locale): String =
-        locale.toLanguageTag().replace("-", "_")
 }
