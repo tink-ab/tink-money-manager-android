c94bcd4980746c25d40f2d6d8f488ae2e8ef09d5
jonathan.persgarden <jonathan.persgarden@tink.com>
2022-01-26 10:10:23 +0100

    Made insight filter dynamic.

--- moneymanager-ui/src/main/java/com/tink/moneymanagerui/insights/repository/InsightsRepository.kt
+++ moneymanager-ui/src/main/java/com/tink/moneymanagerui/insights/repository/InsightsRepository.kt
@@ -2,9 +2,8 @@ package com.tink.moneymanagerui.insights.repository
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
-import com.tink.annotations.PfmScope
 import com.tink.model.insights.Insight
-import com.tink.model.insights.InsightType
+import com.tink.moneymanagerui.insights.viewproviders.InsightViewProviderFactory
 import com.tink.service.insight.InsightService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -17,9 +16,9 @@ import se.tink.android.repository.TinkNetworkError
 import se.tink.commons.livedata.Event
 import javax.inject.Inject
 
-@PfmScope
 class InsightsRepository @Inject constructor(
-    private val insightService: InsightService
+    private val insightService: InsightService,
+    private val insightProviderFactory: InsightViewProviderFactory
 ) {
     private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
@@ -27,7 +26,7 @@ class InsightsRepository @Inject constructor(
         scope.launch {
             val result: ErrorOrValue<List<Insight>> = try {
                 val insights = insightService.listInsights()
-                    .filter { supportedTypes.contains(it.type) }
+                    .filter { insightProviderFactory.provider(it.type) != null }
                 ErrorOrValue(value = insights)
             } catch (error: Throwable) {
                 ErrorOrValue(error = TinkNetworkError(error))
@@ -45,7 +44,7 @@ class InsightsRepository @Inject constructor(
             scope.launch {
                 val result: ErrorOrValue<List<Insight>> = try {
                     val insights = insightService.listArchived()
-                        .filter { supportedTypes.contains(it.type) }
+                        .filter { insightProviderFactory.provider(it.type) != null }
                     ErrorOrValue(value = insights)
                 } catch (error: Throwable) {
                     ErrorOrValue(error = TinkNetworkError(error))
@@ -69,29 +68,4 @@ class InsightsRepository @Inject constructor(
 
     fun refreshInsights() = _insights.update()
     fun refreshArchived() = _archivedInsights.update()
-
-    companion object {
-        private val supportedTypes = listOf(
-            InsightType.ACCOUNT_BALANCE_LOW,
-            InsightType.BUDGET_CLOSE_NEGATIVE,
-            InsightType.BUDGET_CLOSE_POSITIVE,
-            InsightType.BUDGET_OVERSPENT,
-            InsightType.BUDGET_SUCCESS,
-            InsightType.BUDGET_SUGGEST_CREATE_FIRST,
-            InsightType.BUDGET_SUGGEST_CREATE_TOP_CATEGORY,
-            InsightType.BUDGET_SUGGEST_CREATE_TOP_PRIMARY_CATEGORY,
-            InsightType.BUDGET_SUMMARY_ACHIEVED,
-            InsightType.BUDGET_SUMMARY_OVERSPENT,
-            InsightType.DOUBLE_CHARGE,
-            InsightType.LARGE_EXPENSE,
-            InsightType.MONTHLY_SUMMARY_EXPENSES_BY_CATEGORY,
-            InsightType.MONTHLY_SUMMARY_EXPENSE_TRANSACTIONS,
-            InsightType.SINGLE_UNCATEGORIZED_TRANSACTION,
-            InsightType.WEEKLY_SUMMARY_EXPENSES_BY_CATEGORY,
-            InsightType.WEEKLY_SUMMARY_EXPENSES_BY_DAY,
-            InsightType.WEEKLY_SUMMARY_EXPENSE_TRANSACTIONS,
-            InsightType.WEEKLY_UNCATEGORIZED_TRANSACTIONS
-        )
-
-    }
 }
