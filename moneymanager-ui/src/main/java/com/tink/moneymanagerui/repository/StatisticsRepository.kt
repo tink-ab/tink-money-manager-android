8b1e460099360ba223910dd6fdf117b29ef8ef32
Oskar Höjwall <oskar.hojwall@tink.com>
2022-02-14 14:09:19 +0100

    Handle no transactions correctly for current month

--- moneymanager-ui/src/main/java/com/tink/moneymanagerui/repository/StatisticsRepository.kt
+++ moneymanager-ui/src/main/java/com/tink/moneymanagerui/repository/StatisticsRepository.kt
@@ -137,11 +137,17 @@ internal class StatisticsRepository @Inject constructor(
             ?.start
             ?.toDateTime() ?: DateTime.now()
 
+        val endDate = DateTime.now()
+            .withDayOfMonth(backfillStartTime.dayOfMonth)
+            .withHourOfDay(23)
+            .withMinuteOfHour(59)
+            .withSecondOfMinute(59)
+
         val existingPeriodIdentifiers = distinctBy { it.period.identifier }.map { it.period.identifier }
         val monthsMissingTransactions = generateSequence(backfillStartTime) { date ->
             date.plusMonths(1)
         }.takeWhile { iteratedDate ->
-            iteratedDate.isBeforeNow
+            iteratedDate.isBefore(endDate)
         }.filter { iteratedDate ->
             !existingPeriodIdentifiers.contains(iteratedDate.toPeriodIdentifier())
         }.map { iteratedDate ->
@@ -226,7 +232,7 @@ internal class StatisticsRepository @Inject constructor(
             is SuccessState -> {
                 SuccessState(
                     DateTime().let { now ->
-                        periodMapState.data.values.firstOrNull { it.isInPeriod(now) }
+                        periodMapState.data.values.firstOrNull { it.isInPeriod(now) } ?: periodMapState.data.values.maxByOrNull { it.identifier }
                     }
                 )
             }
