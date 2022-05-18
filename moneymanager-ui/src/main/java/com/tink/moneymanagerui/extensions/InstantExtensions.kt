4f869f1550dc132b611dc54faa8c40306fa69f24
Marco Biagini <>
2022-05-18 15:56:35 +0200

    Added an extension function for converting Instant to millisec that can handle Arithmetic exceptions

--- moneymanager-ui/src/main/java/com/tink/moneymanagerui/extensions/InstantExtensions.kt
+++ moneymanager-ui/src/main/java/com/tink/moneymanagerui/extensions/InstantExtensions.kt
@@ -2,5 +2,18 @@ package com.tink.moneymanagerui.extensions
 
 import se.tink.commons.extensions.toDateTime
 import java.time.Instant
+import java.time.LocalDateTime
 
 fun Instant.minusMonths(months: Int) = toDateTime().minusMonths(months.toLong()).toInstant()
+
+fun Instant.toEpochMilliSafe(): Long =
+  try {
+    this.toEpochMilli()
+  } catch (ignore: ArithmeticException) {
+    val now = LocalDateTime.now()
+    if (this.epochSecond < 0) {
+      now.getStartOfYear().millsInLocalTimeZone()
+    } else {
+      now.getEndOfYear().millsInLocalTimeZone()
+    }
+  }
