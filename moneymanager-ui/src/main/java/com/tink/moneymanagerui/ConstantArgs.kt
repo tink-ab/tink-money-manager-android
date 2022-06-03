4d4828c5440f059160e7daf98800194614642b6f
Muhammad Umair <muhammad.shafique@tink.com>
2022-06-03 22:02:53 +0200

    Created ConstantArgs file to keep argument keys at one place

---
+++ moneymanager-ui/src/main/java/com/tink/moneymanagerui/ConstantArgs.kt
@@ -0,0 +1,12 @@
+package com.tink.moneymanagerui
+
+const val ARG_OVERVIEW_FEATURES = "overviewFeatures"
+const val ARG_ACCESS_TOKEN = "accessToken"
+const val ARG_STYLE_RES_ID = "styleResId"
+const val ARG_TRACKER = "tracker"
+const val ARG_BACK_LTR = "backPressedListener"
+const val ARG_IS_OVERVIEW_TOOLBAR_VISIBLE = "isOverviewToolbarVisible"
+const val ARG_FEATURE_SPECIFIC_THEME = "featureSpecificThemes"
+
+const val ARG_FIRST_LAUNCH = "firstLaunch"
+const val ARG_STYLE_RES = "styleRes"
